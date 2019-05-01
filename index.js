5a90cb84add0dfeeb3f34fdfe161d69a41622a0c
Nicolas Marini <nmarini44@gmail.com>
2019-05-01 22:24:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year){
+    return parseInt(year) - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled(){
+      let horizontalDist = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal);
+      let verticalDist = this.beginningLocation.vertical - this.endingLocation.vertical;
+
+      return Math.abs(horizontalDist) + Math.abs(verticalDist);
+  }
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}

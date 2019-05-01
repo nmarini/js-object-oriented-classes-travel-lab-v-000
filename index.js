7d3684e5061a2c516c4a5b22d2d08a0d215f8af2
Nicolas Marini <nmarini44@gmail.com>
2019-05-01 22:03:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ class Driver {
 }
 
 class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
 
 }

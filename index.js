a221d3eb70e3b865ba4c0f1e0285b5d49acca62a
Nicolas Marini <nmarini44@gmail.com>
2019-05-01 22:01:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
     this.startDate = new Date(startDate);
   }
   yearsExperienceFromBeginningOf(year){
-    return year - this.startDate.getYear()
+    return parseInt(year) - this.startDate.getFullYear()
   }
 }
 
